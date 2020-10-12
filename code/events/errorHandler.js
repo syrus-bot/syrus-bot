ec32378e93be716c800262edc610111926a362ed
Nytelife26 <xtylerjrx@gmail.com>
2020-10-13 00:26:54 +0100

    (feat) error message handler

--- code/events/errorHandler.js
+++ code/events/errorHandler.js
@@ -30,5 +30,6 @@ module.exports = class errorEvent extends Event {
 
 	async run(error, errorPayload) {
         console.log(`Error in command ${errorPayload.piece.name}: ${error}`);
+        errorPayload.message.channel.send(`Whoops! Look like there was an error in the command you just ran. Sorry about that!\nPlease can you report this to the Syrus development team in our support server? Here's some debug information to give them:\n\`${errorPayload.piece.name} - ${errorPayload.message.createdTimestamp} - ${error}\``);
 	}
 };
