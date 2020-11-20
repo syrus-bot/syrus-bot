e3c042448664d16f43e039b9e920bc5573ef1793
Nytelife26 <xtylerjrx@gmail.com>
2020-11-20 11:23:04 +0000

    feat(music): add stop
    RE #13

---
+++ src/commands/Music/stop.js
@@ -0,0 +1,43 @@
+/*
+    Syrus - a multipurpose Discord bot, designed to be the best so you don't need the rest.
+    Copyright (C) 2020, Syrus Development Team (Nytelife26 / nytelife@protonmail.com, Logan Heinzelman, ColeCCI and mynameismrtime)
+
+    This file is part of Syrus.
+
+    Syrus is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Syrus is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Syrus.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+const SyrusCommand = require("../../lib/structures/SyrusCommand");
+const { Args, CommandOptions } = require("@sapphire/framework");
+
+module.exports = class ClientCommand extends SyrusCommand {
+
+	constructor(context) {
+		super(context, {
+			name: "stop",
+			aliases: ["clearq", "clearqueue"],
+			description: "music:stop.description"
+		});
+	}
+
+	async run(message, args) {
+		const queue = this.client.music.queues.get(message.guild.id);
+		if (queue.player.playing) {
+			await queue.stop();
+			await queue.clear();
+			return message.sendTranslated("music:stop.stopped");
+		}
+		return message.sendTranslated("music:stop.nothing");
+	}
+};
