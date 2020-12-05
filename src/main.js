a945f58fb041fcdf25067c6ee011cd253b29dea2
Nytelife26 <xtylerjrx@gmail.com>
2020-12-05 20:12:28 +0000

    feat(core): add elegant exit events
    This implements exit events that enable components of the bot to clean up gracefully by listening to
    the `cleanup` event when the process exits. These are triggered by any exit, and special cases are
    made for uncaught exceptions or SIGINT.
    
    RE #34

--- src/main.js
+++ src/main.js
@@ -2,3 +2,17 @@ const Discord = require("discord.js");
 const Manager = new Discord.ShardingManager("./bot.js");
 const config = require("./config.json");
 Manager.spawn(config.shards);
+
+process.on("exit", () => {
+	process.emit("cleanup");
+});
+
+process.on("SIGINT", () => {
+	console.log("\nExiting...");
+	process.exit(2);
+});
+process.on("uncaughtException", (error) => {
+	console.log("Uncaught Exception:");
+	console.error(error);
+	process.exit(99);
+});
