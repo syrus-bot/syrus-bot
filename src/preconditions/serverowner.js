6b3dfe8e208638fc2f6959422a6e198a39463016
Nytelife26 <xtylerjrx@gmail.com>
2020-12-10 21:09:29 +0000

    chore: add note to preconditions for cascade implementation
    RE #25

--- src/preconditions/serverowner.js
+++ src/preconditions/serverowner.js
@@ -2,10 +2,11 @@ const { Precondition } = require("@sapphire/framework");
 
 module.exports = class ClientPrecondition extends Precondition {
 	run(message) {
+		// TODO: add cascade for bot owners
 		const isOwner = message.author.id === message.guild.ownerID;
 		return isOwner ? this.ok() : this.err(
 			this.name,
 			"Only my masters are allowed to execute this command."
 		);
 	}
-}
+};
