cbdbfaf839fa8cbef5a737900098ae86a1c041ff
Nytelife26 <xtylerjrx@gmail.com>
2020-12-19 20:03:58 +0000

    feat(checks): add cascade for dj precondition
    RE #25

--- src/preconditions/DJ.js
+++ src/preconditions/DJ.js
@@ -6,12 +6,18 @@ module.exports = class ClientPrecondition extends Precondition {
 	}
 
 	async run(message) {
-		// TODO: add cascade for moderator
 		const config = await this.client.settings.guild(message.guild.id);
-		const isDj = this.checkRoles(
+		let isDj;
+		isDj = this.checkRoles(
 			message.member.roles.cache,
 			config.djRoles
 		);
+		if (!isDj) {
+			isDj = await this.client.preconditions.find((precondition) => {
+				return precondition.name === "Mod";
+			}).run(message);
+			isDj = isDj.success;
+		}
 		return isDj ? this.ok() : this.error(
 			this.name,
 			"Only DJs or higher are allowed to execute that command."
