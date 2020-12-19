f5c9be620fbabe6d12e57e705de2ed78a7156034
Nytelife26 <xtylerjrx@gmail.com>
2020-12-19 22:07:30 +0000

    refactor(checks): use builtins for owner precondition
    Prior to this, the Owner precondition would import UserError, ok, and err to handle its return
    system. Now, it uses the precondition builtins.

--- src/preconditions/Owner.js
+++ src/preconditions/Owner.js
@@ -1,12 +1,12 @@
 const config = require("../config.json");
-const { err, ok, Precondition, UserError } = require("@sapphire/framework");
+const { Precondition } = require("@sapphire/framework");
 
 module.exports = class ClientPrecondition extends Precondition {
 	run(message) {
 		const isOwner = config.owners.includes(message.author.id);
-		return isOwner ? ok() : err(new UserError(
-			"ownerOnly",
+		return isOwner ? this.ok() : this.err(
+			this.name,
 			"Only my masters are allowed to execute this command."
-		));
+		);
 	}
 };
