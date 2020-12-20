1327e9f9260718d4a8e3778bc1b6fe4e8f4f397b
Nytelife26 <xtylerjrx@gmail.com>
2020-12-20 02:34:19 +0000

    fix(checks): asynchronise owner

--- src/preconditions/Owner.js
+++ src/preconditions/Owner.js
@@ -2,7 +2,7 @@ const config = require("../config.json");
 const { Precondition } = require("@sapphire/framework");
 
 module.exports = class ClientPrecondition extends Precondition {
-	run(message) {
+	async run(message) {
 		const isOwner = config.owners.includes(message.author.id);
 		return isOwner ? this.ok() : this.err(
 			this.name,
