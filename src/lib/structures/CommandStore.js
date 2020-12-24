66bf8d317f1cb9ffb91b2b0fe0953f448998ad8c
Nytelife26 <xtylerjrx@gmail.com>
2020-12-24 19:03:05 +0000

    perf(core): make command categories static
    Prior to this, every time `commands.categories` was accessed, it would dynamically return an array
    computed by iterating through all commands and logging their unique categories. Now, hooks are
    implemented into the store, so the array is static and recomputed only when a command is loaded or
    unloaded. This cuts access times approximately in half.

--- src/lib/structures/CommandStore.js
+++ src/lib/structures/CommandStore.js
@@ -41,8 +41,4 @@ module.exports = class SyrusCommandStore extends BaseAliasStore {
 			(category) => this.fetchCategory(category)
 		);
 	}
-
-	get categories() {
-		return Array.from(new Set(this.map((command) => command.category)));
-	}
 };
